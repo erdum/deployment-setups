6aeafc162513021d194e8f5b3e10477fadf2453d
Erdum <erdumadnan@gmail.com>
2025-03-07 12:38:56 +0500

    Feat: Queue worker implemented

--- elasticbeanstalk-docker-deployment/.platform/hooks/postdeploy/run_postdeploy.sh
+++ elasticbeanstalk-docker-deployment/.platform/hooks/postdeploy/run_postdeploy.sh
@@ -6,4 +6,8 @@ sleep 5
 # Run composer install inside the running container
 sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh -c 'composer install'
 
+# Set correct permissions
 sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh -c 'chmod 777 -R /var/www/html/storage/ /var/www/html/bootstrap/cache/'
+
+# Restart Supervisor to apply new config
+sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh -c 'supervisorctl reread && supervisorctl update && supervisorctl start all'
