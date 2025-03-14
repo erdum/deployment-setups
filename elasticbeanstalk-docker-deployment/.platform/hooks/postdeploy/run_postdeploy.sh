952a869ac07c0c9d3aca41d0d5ce7e007ebcdf2a
Erdum <erdumadnan@gmail.com>
2025-03-14 11:30:04 +0500

    Update run_postdeploy.sh

--- elasticbeanstalk-docker-deployment/.platform/hooks/postdeploy/run_postdeploy.sh
+++ elasticbeanstalk-docker-deployment/.platform/hooks/postdeploy/run_postdeploy.sh
@@ -11,3 +11,8 @@ sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh
 
 # Restart Supervisor to apply new config
 sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh -c 'supervisorctl reread && supervisorctl update && supervisorctl start all'
+
+# If getting max_allowed_stack_size error
+# sudo docker exec $(sudo docker ps -q -f "ancestor=aws_beanstalk/current-app") sh -c 'echo "zend.max_allowed_stack_size = -1" > /usr/local/etc/php/conf.d/custom-stack.ini'
+
+# sudo docker restart $(docker ps -q -f "ancestor=aws_beanstalk/current-app")
